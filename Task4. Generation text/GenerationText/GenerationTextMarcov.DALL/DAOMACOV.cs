48b30b56d48f1fb3f9f556197e89b95a5b5d2d4e
isaikin <IsAikinV1995>
2016-04-27 10:26:28 +0300

    no message

---
+++ Task4. Generation text/GenerationText/GenerationTextMarcov.DALL/DAOMACOV.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenerationTextMarcov.DAL
+{
+    public class DAOMACOV
+    {      
+            private static string words;
+
+            public DAOMACOV()
+            {
+                words = this.GetWords();
+            }
+
+            public void AddWords(string path)
+            {
+                var text = new List<string>();
+
+                using (StreamReader input = new StreamReader(path))
+                {
+                    var temptext = input.ReadToEnd();
+                    var separator = this.GetSeparator(temptext);
+
+                    text = temptext.Split(separator.ToArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
+                    text = text.Select(word => this.ClearWord(word)).ToList();
+                }
+                var result = new StringBuilder();
+                foreach (var item in text)
+                {
+                    result.AppendFormat($"{item }");
+                }
+                words = result.ToString();
+
+                this.SaveWords();
+            }
+
+            public void AddWords(List<string> text)
+            {
+
+                text = text.Select(word => this.ClearWord(word)).ToList();
+
+                if (!words.ContainsKey(text[0].ToLower()))
+                {
+                    words.Add(text[0].ToLower(), new List<string>());
+                }
+
+                for (int i = 0; i < text.Count - 1; i++)
+                {
+                    if (!words.ContainsKey(text[i + 1].ToLower()))
+                    {
+                        var tempList = new List<string>();
+                        tempList.Add(text[i].ToLower());
+                        words.Add(text[i + 1].ToLower(), tempList);
+                        if (!words.ContainsKey(text[i].ToLower()))
+                        {
+                            words.Add(text[i].ToLower(), new List<string>());
+                        }
+                    }
+                    else
+                    {
+                        if (!words[text[i + 1].ToLower()].Contains(text[i].ToLower()))
+                        {
+                            words[text[i + 1].ToLower()].Add(text[i].ToLower());
+                        }
+                    }
+                }
+
+                if (!words.ContainsKey(text.Last().ToLower()))
+                {
+                    words.Add(text.Last().ToLower(), new List<string>());
+                }
+
+                this.SaveWords();
+            }
+
+            public string Getwords()
+            {
+              return words.ToString() ;
+            }
+
+            public string ClearWord(string temp)
+            {
+                string outs = "";
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    if (!Char.IsControl(temp[i]))
+                    {
+                        outs += temp[i];
+                    }
+                }
+                return outs.Trim(' ');
+            }
+
+            private string GetWords()
+            {
+                if (!File.Exists("inputMarcov.txt"))
+                {
+                    File.Create("inputMarcov.txt");
+                }
+
+                 var result = string.Empty;
+                using (StreamReader input = new StreamReader("inputWords.txt"))
+                {
+                    result = input.ReadToEnd();
+                    
+                }
+
+                return result;
+            }
+
+            public List<string> GetSeparator(string text)
+            {
+                var separator = new List<string>();
+                foreach (var tempChar in text)
+                {
+                    if ((char.IsSeparator(tempChar) || char.IsPunctuation(tempChar)) && !separator.Contains(tempChar.ToString()))
+                    {
+                        separator.Add(tempChar.ToString());
+                    }
+                }
+
+                return separator;
+            }
+
+            private void SaveWords()
+            {
+                using (StreamWriter output = new StreamWriter("inputWords.txt"))
+                {
+                output.WriteLine(words);
+                }
+            }
+        }
+    }
+}
+}
